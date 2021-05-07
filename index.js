0b5622a135f91a4a9f3e3671e3ab7f11be8ae985
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-05-07 16:47:41 -0500

    port developer

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import express from 'express'
-import phoneRouter from './routes/phoneRoutes.js'
-import cors from 'cors'
+import express from "express";
+import phoneRouter from "./routes/phoneRoutes.js";
+import cors from "cors";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use('/',phoneRouter)
+app.use("/", phoneRouter);
 
-
-app.listen(3333, ()=>{
-    console.log('Api iniciada')
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Api iniciada");
+});
